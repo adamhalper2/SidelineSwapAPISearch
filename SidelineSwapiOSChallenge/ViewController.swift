0777d2850c2e9fab9bc720771cdb418190649bed
Adam Halper <Adamhalper8@gmail.com>
2020-09-27 11:26:01 -0700

    Initial Commit

---
+++ SidelineSwapiOSChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SidelineSwapiOSChallenge
+//
+//  Created by Adam Halper on 9/27/20.
+//  Copyright © 2020 Adam Halper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
